bd8e654ce8eb9dbff719e2d9cffb0c2d36853aae
niels9001 <niels.laute@live.nl>
2022-04-06 09:54:41 +0200

    Initialize commit

---
+++ MSFTLogo/MSFTLogo/AnimatableQuadrant.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace MSFTLogo
+{
+    public sealed partial class AnimatableQuadrant : UserControl
+    {
+        public AnimatableQuadrant()
+        {
+            this.InitializeComponent();
+        }
+        public string ImagePath
+        {
+            get => (string)GetValue(ImagePathProperty);
+            set => SetValue(ImagePathProperty, value);
+        }
+
+        public static readonly DependencyProperty ImagePathProperty = DependencyProperty.Register("ImagePath", typeof(string), typeof(AnimatableQuadrant), new PropertyMetadata(string.Empty));
+
+        public TimeSpan? Delay
+        {
+            get => (TimeSpan?)GetValue(DelayProperty);
+            set => SetValue(DelayProperty, value);
+        }
+
+        public static readonly DependencyProperty DelayProperty = DependencyProperty.Register("Delay", typeof(TimeSpan?), typeof(AnimatableQuadrant), new PropertyMetadata(new TimeSpan(0)));
+
+
+        public SolidColorBrush OverlayColor
+        {
+            get => (SolidColorBrush)GetValue(OverlayColorProperty);
+            set => SetValue(OverlayColorProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayColorProperty = DependencyProperty.Register("OverlayColor", typeof(SolidColorBrush), typeof(AnimatableQuadrant), new PropertyMetadata(Colors.White));
+
+        public void ZoomIn()
+        {
+            ImageHolder.Visibility = Visibility.Visible;
+            ZoomInAnimation.Start();
+        }
+
+        public void ZoomOut()
+        {
+            ImageHolder.Visibility = Visibility.Collapsed;
+            ZoomOutAnimation.Start();
+        }
+    }
+}
